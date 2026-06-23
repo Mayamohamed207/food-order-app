ddb992637135d1c2ecea5aadfa8426afcb3cecf7
Mayamohamed207 <mayamohamed364@gmail.com>
2026-06-23 22:23:11 +0300

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,48 @@
+export interface MenuItem {
+  id: string;
+  name_en: string;
+  name_ar: string;
+  description_en?: string;
+  description_ar?: string;
+  price: number;
+  image_url: string;
+  category_id: string;
+  is_available: boolean;
+}
+
+export interface Category {
+  id: string;
+  name_en: string;
+  name_ar: string;
+}
+
+export interface CartItem {
+  id: string;
+  name_en: string;
+  name_ar: string;
+  price: number;
+  image_url: string;
+  quantity: number;
+}
+
+export type PaymentMethod = 'cash' | 'online';
+
+export type OrderStatus = 'pending' | 'preparing' | 'on_the_way' | 'delivered' | 'cancelled';
+
+export interface OrderItem {
+  product_id: string;
+  name_en: string;
+  name_ar: string;
+  quantity: number;
+  price: number;
+}
+
+export interface Order {
+  id: string;
+  user_id: string;
+  items: OrderItem[];
+  total: number;
+  payment_method: PaymentMethod;
+  status: OrderStatus;
+  created_at: string;
+}
